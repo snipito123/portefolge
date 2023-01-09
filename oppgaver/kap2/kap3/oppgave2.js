7419f7a0aefdea42dd869433f0ac082bd3f6aa4b
snipito123 <mathiasbakken05@gmail.com>
2023-01-09 16:15:34 +0100

    arbeids oppgaver

---
+++ oppgaver/kap2/kap3/oppgave2.js
@@ -0,0 +1,21 @@
+function tilfeldigHilsen() {
+    console.log("hei");
+}
+function bilTilOslo() {
+    console.log("Fremme i Oslo.");
+}
+function bilTilDombås() {
+    console.log("Fremme på Dombås");
+}
+function bil(sted, metode) {
+    console.log("kjører " + metode + " til " + sted);
+}
+function hilsen(juhu) {
+    console.log(juhu);
+}
+bil("Oslo", "lynraskt");
+hilsen("halla balla");
+bil("Dombås", "som ein snegle");
+bil("Haugesund", "i rasende fart");
+hilsen("hei");
+bil("Dombås", "hoppende");
