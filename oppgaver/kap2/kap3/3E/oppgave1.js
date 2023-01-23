7ed9df977f3a9c1beb299a0b571979a397d6fd16
snipito123 <mathiasbakken05@gmail.com>
2023-01-23 12:27:31 +0100

    oppgaver

--- oppgaver/kap2/kap3/3E/oppgave1.js
+++ oppgaver/kap2/kap3/3E/oppgave1.js
@@ -1,2 +1,7 @@
 let tall = [0, 1, 2, 3, 4, 6, 7, 8, 9, 10];
 let nrnamn = ["nr 0", "nr 1", "nr 2", "nr 4", "nr 6", "nr 7", "nr 8", "nr 9", "nr 10"];
+console.log(tall[2]);
+console.log(tall[4]);
+console.log(tall[5]);
+console.log(tall[7]);
+console.log(tall[9]);
