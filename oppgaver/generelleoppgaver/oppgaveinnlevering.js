c9aad26d439e51d04b5a612b78a0885fc748ef13
snipito123 <mathiasbakken05@gmail.com>
2023-01-28 17:28:34 +0100

    oppgave innlevering i teams

--- oppgaver/generelleoppgaver/oppgaveinnlevering.js
+++ oppgaver/generelleoppgaver/oppgaveinnlevering.js
@@ -6,5 +6,16 @@ let informasjonsteknologi1 = {
     antallElever: 24
   };
 
-  spraak.push("javascript");
-  console.log(spraak)
+    informasjonsteknologi1.spraak.push("javascript");
+
+    informasjonsteknologi1.antallElever = "7";
+
+    delete informasjonsteknologi1.timetall;
+
+    for (let egenskap in informasjonsteknologi1.spraak) {
+         console.log(informasjonsteknologi1.spraak);
+    }
+   
+    for (let egenskap in informasjonsteknologi1) {
+        console.log([informasjonsteknologi1]);
+   }
