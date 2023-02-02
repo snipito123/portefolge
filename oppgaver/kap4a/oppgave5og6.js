5cb4a056668493e16783e88e9465daa27f042b38
snipito123 <mathiasbakken05@gmail.com>
2023-02-02 11:08:00 +0100

    oppgaver i kap 4

---
+++ oppgaver/kap4a/oppgave5og6.js
@@ -0,0 +1,17 @@
+/* oppgave 5
+*/
+//a
+let alleh1El = document.querySelectorAll("h1");
+//b
+let allestyleE2 = document.querySelectorAll("style");
+//c
+let allebodyerE3 = document.querySelectorAll("body");
+//d
+let alletitlerE4 = document.querySelectorAll("title");
+
+/*nå løses oppgave 6 på følgende måte, kommer  under
+*/
+console.log(alleh1El);
+console.log(allestyleE2);
+console.log(allebodyerE3);
+console.log(alletitlerE4);
