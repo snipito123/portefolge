8165670f1a0747c1102f3a1300ed9d67c4230e21
snipito123 <mathiasbakken05@gmail.com>
2023-02-06 12:47:57 +0100

    oppgave 7 og 8

---
+++ oppgaver/kap4a/oppgave7og8.js
@@ -0,0 +1,15 @@
+console.log(listepunkt1)
+console.log(listepunkt2)
+console.log(listepunkt3)
+console.log(listepunkt4)
+/*
+oppgave 7 limte inn koden å skreiv ut denn der
+*/
+let listepunktEl = document.querySelector("li");
+console.log(listepunktEl);
+/*
+oppgave 8, skriv ut alle listepunkter
+*/
+
+let listepunktall = document.querySelectorAll("li");
+console.log(listepunktall)
