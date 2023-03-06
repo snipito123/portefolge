ab6151cb4d6ce8bd28b826e81c3b91397139fc5b
snipito123 <mathiasbakken05@gmail.com>
2023-03-06 13:23:40 +0100

    fleire oppgaver

---
+++ oppgaver/kap4a/blandede.oppgaver2.js
@@ -0,0 +1,33 @@
+let personer = [
+    { ekspedisjon: "Apollo 11", 
+    årstall: "20. juli 1969",
+    deltagere: "Neil Armstrong,Buzz Aldrin"
+    },
+    { ekspedisjon: "Apollo 12", 
+    årstall: "19. november 1969",
+    deltagere: "Pete Conra, Alan Bean"
+    },
+    { ekspedisjon: "Apollo 14", 
+    årstall: "5. februar 1971",
+    deltagere: "Alan Shepard, Edgar Mitchell"
+    },
+    { ekspedisjon: "Apollo 15", 
+    årstall: "31. juli 1971",
+    deltagere: "David R. Scott, James Irwin"
+    },
+    { ekspedisjon: "Apollo 16", 
+    årstall: "21. april 1972",
+    deltagere: "John Young, Charles Duke"
+    }, 
+    { ekspedisjon: "Apollo 17", 
+    årstall: "11. desember 1972",
+    deltagere: "Eugene Cernan, Harrison Schmitt"
+    },
+
+  ];
+
+  for (let egenskap in personer) {
+    console.log(årstall);
+    console.log(ekspedisjon);
+    console.log(deltagere);
+  }
